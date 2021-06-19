c40251e8fdae39491894f93a2d6fbb803a603d81
Woosung Choi <ws_choi@korea.ac.kr>
2021-06-19 11:43:06 +0900

    Update libs.js

--- javascripts/libs.js
+++ javascripts/libs.js
@@ -1,7 +1,7 @@
-import "modernizr.foundation.js";
-import 'jquery-2.1.0.js';
-import 'terrific-1.1.1.js';
-import 'jquery.url.js';
-import 'jquery.cookie.js';
-import 'json2.js';
-import 'foundation.js';
+<script type="text/javascript" src="modernizr.foundation.js"></script>
+<script type="text/javascript" src="jquery-2.1.0.js"></script>
+<script type="text/javascript" src="terrific-1.1.1.js"></script>
+<script type="text/javascript" src="jquery.url.js"></script>
+<script type="text/javascript" src="jquery.cookie.js"></script>
+<script type="text/javascript" src="json2.js"></script>
+<script type="text/javascript" src="foundation.js"></script>
